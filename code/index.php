fc2b095bb97808063f9d7b332659b784b2b60145
tranquansp <tranquansp@gmail.com>
2016-07-24 15:14:44 +0700

    tam on x5
    tam on x5

---
+++ code/index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>.::. Đông Trùng Hạ Thảo X5 .::.</title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                
+				<link href = "public/css/bootstrap.min.css" rel = "stylesheet">
+				<link href = "public/css/x5i.css" rel = "stylesheet"  type="text/css" >
+				<link type="text/css" rel="stylesheet"  href="public/css/styleslider.css" />
+				<script src = "public/js/jquery-2.1.1.min.js"></script>
+				<script src = "public/js/bootstrap.js"></script>       
+				<script src = "public/js/modernizr.custom.28468.js"></script>   
+				<script type="text/javascript" src="public/js/jquery.cslider.js"></script>		
+				
+				<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+				<meta name="description" content="Parallax Content Slider with CSS3 and jQuery" />
+				<meta name="keywords" content="slider, animations, parallax, delayed, easing, jquery, css3, kendo UI" />
+				<meta name="author" content="Codrops" />
+				<link rel="shortcut icon" href="../favicon.ico"> 
+				<link href='http://fonts.googleapis.com/css?family=Economica:700,400italic' rel='stylesheet' type='text/css'>
+				
+        </head>
+        <body>
+                <div class = "navbar navbar-inverse navbar-fixed-top" role="navigation" style="background-color: #fef7e4;">
+                <div class="navbar-inner ">
+								<!-- LOGO -->
+                                 <div class = "container-fluid" style="max-width: 100%">
+                                     <a class="navbar-brand" rel="home" href="/" title="Cùng đi xem phim">
+										<img style="margin-top: -7px;" src="public/image/template/logo.png">
+									</a>
+									
+								<!-- Navigator Bar -->
+                                <button class = "navbar-toggle" style="border-color: #fef7e4;" data-toggle = "collapse" data-target = ".navHeaderCollapse" >
+                                        <span class = "icon-bar" style="background-color: #d3af49;"></span>
+                                        <span class = "icon-bar" style="background-color: #d3af49;"></span>
+                                        <span class = "icon-bar" style="background-color: #d3af49;"></span>
+                                </button>
+
+                                <div class = "collapse navbar-collapse navHeaderCollapse">
+                                        <ul class = "nav navbar-nav navbar-right">
+                                                <li class="menuactive"><a href = "/" title="Sản phẩm Đông Trùng Hạ Thảo "> Sản Phẩm </a></li>
+                                                <li class="menunonactive"><a href = "/" title="Chuyển giao công nghệ trồng Đông Trùng Hạ Thảo ">Chuyển giao công nghệ </a></li>
+                                                <li class="menunonactive"><a href = "/" title="Giới thiệu X5">Giới Thiệu</a></li>                             
+												<li class="menunonactive"><a href = "/" title="Giới thiệu X5">Tin Tức</a></li>
+												<li class="menunonactive"><a href = "/" title="Giới thiệu X5">Liên Hệ</a></li>
+                                        </ul>
+                               
+                                </div>
+                               
+                        </div>
+                        </div>
+                </div>
+ 
+        <div class = "container" style="margin-top:50px;padding-left:0px;padding-right:0px;width:100%">
+                     <div class = "row">                
+						<div class = "col-lg-12" style="text-align:center;">                      
+								<div class = "panel panel-default">
+									<div class = "panel-body">		
+									<div class="container_slider">
+										<div id="da-slider" class="da-slider">
+											<div class="da-slide">
+												<h2>Đông Trùng Hạ Thảo X5</h2>
+												<p>Thảo dược quý hiếm có nguồn gốc từ thiên nhiên.</p>
+												<a href="#" class="da-link">Tìm hiểu thêm</a>
+												<div class="da-img"><img src="public/image/slider/1.png" alt="image01" /></div>
+											</div>
+											<div class="da-slide">
+												<h2>Chuyển Giao Công Nghệ</h2>
+												<p>X5 đã sẵn sàng chuyển giao công nghệ nuôi trồng Đông Trùng Hạ Thảo.</p>
+												<a href="#" class="da-link">Tìm Hiểu Thêm</a>
+												<div class="da-img"><img src="public/image/slider/2.png" alt="image02" /></div>
+											</div>
+											<div class="da-slide">
+												<h2>Chế Biến</h2>
+												<p>Mát dịu ngày hè với Đông Trùng Hạ Thảo hầm yến.</p>
+												<a href="#" class="da-link">Tìm hiểu thêm</a>
+												<div class="da-img"><img src="public/image/slider/3.png" alt="image03" /></div>
+											</div>
+											<div class="da-slide">
+												<h2>Trà Thảo Mộc X5</h2>
+												<p>X5 vừa chế biến thành công loại trà dinh dưỡng tốt cho sức khỏe.</p>
+												<a href="#" class="da-link">Tìm hiểu thêm</a>
+												<div class="da-img"><img src="public/image/slider/4.png" alt="image04" /></div>
+											</div>
+											<nav class="da-arrows">
+												<span class="da-arrows-prev"></span>
+												<span class="da-arrows-next"></span>
+											</nav>
+										</div>
+									</div>  	
+
+									 
+									</div>
+								</div>    				              
+						</div>
+						
+		
+		<script type="text/javascript">
+			$(function() {
+			
+				$('#da-slider').cslider({
+					autoplay	: true,
+					bgincrement	: 450
+				});
+			
+			});
+		</script>
+		
+<!--
+col-xs-1 col-sm-2
+-->
+
+						<div class = " col-lg-12">						
+							<div class = "list-group" style="margin-left: -2px;margin-right: -2px;">
+									<div id="row">
+											<div class = " col-xs-12 col-sm-6 col-md-4 col-lg-4"  style="padding-left: 2px;padding-right: 2px;">
+												<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+													<h4 class = "list-group-item-heading">Sản Phẩm</h4>
+													<hr style="margin-top:10px;margin-bottom:12px;"/>
+													<p class = "list-group-item-text">Sản phẩm tươi ,khô của <a href="/"><strong>X5</strong></a> bạn nhé</p>
+												</div>
+											</div>
+											
+											<div class = " col-xs-12 col-sm-6 col-md-4 col-lg-4"  style="padding-left: 2px;padding-right: 2px;">
+												<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+													<h4 class = "list-group-item-heading">Chuyển Giao Công Nghệ</h4>
+													<hr style="margin-top:10px;margin-bottom:12px;"/>
+													<p class = "list-group-item-text">X5 thành lập phòng nghiên cứu CNSH <a href="/"><strong>X-Lab</strong></a></p>
+												</div>
+											</div>
+											<div class = " col-xs-12 col-sm-6 col-md-4 col-lg-4"  style="padding-left: 2px;padding-right: 2px;">
+												<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+													<h4 class = "list-group-item-heading">Tin Tức</h4>
+													<hr style="margin-top:10px;margin-bottom:12px;"/>
+													<p class = "list-group-item-text">Phát hiện giống nấm mới trên cao nguyên Lâm Viên <a href="/"></p>
+												</div>
+											</div>
+										<!-- end row-->	
+									</div>
+							</div>							
+						</div>
+                       
+                       	<div class = " col-lg-12">						
+							<div class = "list-group" style="margin-left: -2px;margin-right: -2px;">
+									<div id="row">
+											<div class = " col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-left: 2px;padding-right: 2px;">
+												<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+													<h1>Footer Site Map</h1>
+												</div>
+											</div>
+											<div class = " col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-left: 2px;padding-right: 2px;">
+											<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+												<h1>Footer Contact</h1>
+												</div>
+											</div>
+											<div class = " col-xs-12 col-sm-12 col-md-4 col-lg-4" style="padding-left: 2px;padding-right: 2px;">
+											<div class = "list-group-item" style="margin-bottom: 5px;background-color:#ffffff;text-align:center;">
+												<h1>Footer Gmap</h1>
+												</div>
+											</div>
+										<!-- end row-->	
+									</div>
+							</div>							
+						</div>
+                       
+                       
+					</div>  
+			</div>
+
+            
+            <!-- Contact Page-->
+			<div class = "modal fade" id = "contact" role = "dialog">
+				<div class = "modal-dialog">
+					<div class = "modal-content">
+						<div class = "modal-header">
+							<h4>Contact Tech Site</h4>
+						</div>
+						<div class = "modal-body">
+							<p>Test Trang Lien He</p>
+						</div>
+						<div class = "modal-footer">
+					<a class = "btn btn-primary" data-dismiss = "modal">Dong Y</a>    
+							<a class = "btn btn-default" data-dismiss = "modal">Close</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+				<!--		
+			<div class = "navbar navbar-default bg-faded">
+					<div class = "container">
+							<p class = "navbar-text pull-left"><a href = "#contact" data-toggle="modal">Contact</a> · Giới thiệu · FAQ · Điều khoản · RSS · Cách vào Facebook </p>
+					</div>
+			</div>  
+-->			
+
+        </body>
+</html>
